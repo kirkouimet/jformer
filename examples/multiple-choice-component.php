0dd652d1942be4ffee0113043492bfd11f568ba7
Joe Huss <detain@interserver.net>
2014-05-04 01:09:26 -0400

    Couple style changes i needed for various reasons and dropped the locally included jquery and using one on the jquery cdn

--- examples/multiple-choice-component.php
+++ examples/multiple-choice-component.php
@@ -6,7 +6,8 @@
         <link rel="stylesheet" type="text/css" href="../styles/jformer.css" />
 
         <!-- jQuery JavaScript -->
-        <script type="text/javascript" src="../scripts/jquery-1.5.2.min.js"></script>
+        <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 
         <!-- jFormer JavaScript -->
         <script type="text/javascript" src="../scripts/JFormerUtility.js"></script>
